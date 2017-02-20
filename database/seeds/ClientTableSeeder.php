3158687989e238c422bb43748051ae75bd893cba
LEANDRO\Leandro <leandro-web@hotmail.com>
2017-02-20 10:06:43 -0300

    Subindo o Laravel

---
+++ database/seeds/ClientTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ClientTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \CursoLaravel\Client::truncate(); 
+        factory(\CursoLaravel\Client::class, 10)->create();
+    }
+}
